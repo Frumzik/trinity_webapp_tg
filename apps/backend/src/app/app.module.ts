4300fde3c8bd6f85b2e98c7c59bbd5f215088e91
Алексей Кесслер <aleksejkessler@Aleksejs-MacBook-Air.local>
2025-10-09 23:59:42 +0300

    feat: add .env files for staging

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppService } from './app.service';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // чтобы ConfigModule был доступен везде
-      ignoreEnvFile: true,
+      envFilePath: 'apps/backend/.dev.env'
     }),
   ],
   controllers: [AppController],
