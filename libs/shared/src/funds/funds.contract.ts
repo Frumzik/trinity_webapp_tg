cab1c09a444f8f23424264309bc528e7f8a4bcf1
LightninG8 <tov.kessler@yandex.ru>
2025-11-12 13:02:39 +0300

    fix: banner dto

--- libs/shared/src/funds/funds.contract.ts
+++ libs/shared/src/funds/funds.contract.ts
@@ -1,14 +1,28 @@
-import { FundType } from "./funds.interface.js";
+import { IsString, IsEnum, IsNumber, IsBoolean, IsNotEmpty } from 'class-validator';
+import { FundType } from './funds.interface.js';
 
 export class FundCreateRequestDto {
+  @IsString()
+  @IsNotEmpty()
   title!: string;
+
+  @IsEnum(FundType)
   type!: FundType;
 }
 
 export class ReserveFundItemCreateRequestDto {
+  @IsNumber()
   userId!: number;
+
+  @IsNumber()
   sum!: number;
+
+  @IsNumber()
   stage!: number;
+
+  @IsNumber()
   stageLevel!: number;
+
+  @IsBoolean()
   isReturned!: boolean;
 }
