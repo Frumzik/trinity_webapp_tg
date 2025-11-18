92af29f6323a088937e8d0fa6846c5e1d3f475d8
ko3ap <ko3ap@MacBook-Pro-ko3ap.local>
2025-11-18 20:01:52 +0500

    fix ref message

--- apps/webapp/src/pages/detailing/index.tsx
+++ apps/webapp/src/pages/detailing/index.tsx
@@ -71,7 +71,6 @@ const Index = () => {
       (appUserId ? `?startapp=${appUserId}` : "");
     const share = new URL("https://t.me/share/url");
     share.searchParams.set("url", botDeepLink);
-    share.searchParams.set("text", "Присоединяйся к проекту ✨");
     return share.toString();
   }, [u]);
 
