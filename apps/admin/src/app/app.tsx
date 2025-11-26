242c45214708b7a392f5841a0a484e7177ba0094
LightninG8 <tov.kessler@yandex.ru>
2025-11-26 12:30:04 +0300

    feat: add admin cms auth

--- apps/admin/src/app/app.tsx
+++ apps/admin/src/app/app.tsx
@@ -1,52 +1,17 @@
-// Uncomment this line to use CSS modules
-// import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
+import { Admin, Resource, ListGuesser } from "react-admin";
+import simpleRestProvider from "ra-data-simple-rest";
+import { authProvider } from "./authProvider";
+import { httpClient } from "./httpClient";
 
-import { Route, Routes, Link } from 'react-router-dom';
+const dataProvider = simpleRestProvider(import.meta.env.VITE_API_URL, httpClient);
 
-export function App() {
+export default function App() {
   return (
-    <div>
-      <NxWelcome title="@trinity/admin" />
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
-      </Routes>
-      {/* END: routes */}
-    </div>
+    <Admin
+      dataProvider={dataProvider}
+      authProvider={authProvider}
+    >
+      <Resource name="posts" list={ListGuesser} />
+    </Admin>
   );
 }
-
-export default App;
