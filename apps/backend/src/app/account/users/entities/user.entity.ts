2bae6e222d0e8f78f643551f5b0635021be93a78
LightninG8 <tov.kessler@yandex.ru>
2025-12-22 20:45:30 +0300

    fix

--- apps/backend/src/app/account/users/entities/user.entity.ts
+++ apps/backend/src/app/account/users/entities/user.entity.ts
@@ -136,7 +136,7 @@ export class UserEntity implements IUser {
   }
 
   public updateBalance(balance: number) {
-    this.balance = balance;
+    this.balance = Math.round(balance * 1000) / 1000;
 
     return this;
   }
