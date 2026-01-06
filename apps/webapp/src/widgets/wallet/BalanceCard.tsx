cdb1a3bf45af171d0b00c38a63b343bbe6ee81b1
ko3ap <ko3ap@MacBook-Pro-ko3ap.local>
2026-01-06 17:11:45 +0500

    add button in history page

--- apps/webapp/src/widgets/wallet/BalanceCard.tsx
+++ apps/webapp/src/widgets/wallet/BalanceCard.tsx
@@ -34,6 +34,16 @@ export default function BalanceCard({ amount, onDeposit, onWithdraw }: Props) {
           Добавить ОМ
         </button>
       </NavLink>
+        <NavLink to="/transfers">
+        <button className="balance__btn" onClick={onDeposit}>
+          Внутренний перевод
+        </button>
+      </NavLink>
+        <NavLink to="/wallet">
+        <button className="balance__btn" onClick={onDeposit}>
+          Добавить ОМ за ₽
+        </button>
+      </NavLink>
 
       </div>
     </div>
